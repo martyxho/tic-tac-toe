2fac19918f259405600580372eb1426c8cd59610
Marty <maruri2424@gmail.com>
2022-04-04 01:24:22 -0700

    added click box to change marker

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const gameboard = (() => {
-  const gameArr = ['X','X', 'X', 'O', 'O', 'O', 'X', 'O', 'X'];
-
+  const gameArr = [];
   return {gameArr};
 })();
 
 const displayController = (() => {
-  const container = document.querySelector('.container');
   const renderDisplay = (arr) => {
     arr.forEach((e, i) => {
       const div = document.getElementById(i);
       div.textContent = e;
     });
   };
-  
   return {renderDisplay};
 })();
 
+const player = (marker) => {
+  const getMarker = () => marker;
+  return {getMarker};
+};
+
 const game = (() => {
+  const p1 = player('X');
+  const p2 = player('O');
+  let currentPlayer = p1;
+  function markBox(i) {
+    gameboard.gameArr[i] = currentPlayer.getMarker();
+    displayController.renderDisplay(gameboard.gameArr);
+    changePlayer();
+  }
+  function changePlayer() {
+    if (currentPlayer === p1) {
+      currentPlayer = p2;
+    } else {
+      currentPlayer = p1;
+    }
+  }
+  const setBoxListeners = (() => {
+    for (let i = 0; i < 9; i++) {
+      const div = document.getElementById(i);
+      div.addEventListener('click', markBox.bind(null, i), {once : true});
+    }
+  })();
+  
   displayController.renderDisplay(gameboard.gameArr);
 })();
 
-const player = () => {
-
-};

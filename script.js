b0a6c506572d96c2a71864edf3e7511aa4caac84
Marty <maruri2424@gmail.com>
2022-04-05 22:34:28 -0700

    set end game functionality

--- script.js
+++ script.js
@@ -22,10 +22,47 @@ const game = (() => {
   const p1 = player('X');
   const p2 = player('O');
   let currentPlayer = p1;
-  function markBox(i) {
+  function markBox(e) {
+    const i = e.target.id;
     gameboard.gameArr[i] = currentPlayer.getMarker();
     displayController.renderDisplay(gameboard.gameArr);
     changePlayer();
+  }
+  function checkGame() {
+    if (checkWin() || checkEnd()) {
+      endGame();
+    } 
+  }
+  function checkWin() {
+    const gArr = gameboard.gameArr;
+    let checks = [[0 , 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < checks.length; i++) {
+      if (checkRow(gArr, checks[i][0], checks[i][1], checks[i][2])) {
+        return true;
+      } 
+    }
+    return false;
+  }
+  function checkRow(arr, a, b, c) {
+    if (arr[a] == undefined || arr[b] == undefined || arr[c] == undefined) {
+      return false;
+    }
+    const result = (arr[a] == arr[b] && arr[b] == arr[c])
+    return result;
+  }
+  function checkEnd() {
+    return Object.values(gameboard.gameArr).length === 9;
+  }
+  function endGame() {
+    const content = document.querySelector('.content');
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Game Over';
+    content.appendChild(h1);
+    for (let i = 0; i < 9; i++) {
+      const div = document.getElementById(i);
+      div.removeEventListener('click', markBox, {once : true});
+      div.removeEventListener('click', checkGame, {once : true});
+    }
   }
   function changePlayer() {
     if (currentPlayer === p1) {
@@ -37,7 +74,8 @@ const game = (() => {
   const setBoxListeners = (() => {
     for (let i = 0; i < 9; i++) {
       const div = document.getElementById(i);
-      div.addEventListener('click', markBox.bind(null, i), {once : true});
+      div.addEventListener('click', markBox, {once : true});
+      div.addEventListener('click', checkGame, {once : true});
     }
   })();
   
